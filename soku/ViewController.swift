d8d3e9d966b7f64fc9e0227a23084d2d1e9f87d7
den0206 <mac.pop@mineo.jp>
2020-08-01 13:34:51 +0900

    Initial Commit

---
+++ soku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soku
+//
+//  Created by 酒井ゆうき on 2020/08/01.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
